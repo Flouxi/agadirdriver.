eb9ade7111a826222bf1bd3c6870e3d8d44ae15d
Flouxi <125490814+Flouxi@users.noreply.github.com>
2026-07-24 18:04:36 +0000

    done

--- src/lib/brand-images.ts
+++ src/lib/brand-images.ts
@@ -1,11 +1,7 @@
-import logoAsset from '../assets/1784332092725-019f7279-84fc-70bd-9092-0c7cecb60bae-removebg-preview.png.asset.json';
-import travelCoupleImg from '../assets/images/travel_couple.jpg.asset.json';
-import luxuryVanImg from '../assets/images/luxury_van.jpg.asset.json';
-
-export const AGADIR_DRIVER_LOGO_URL = logoAsset.url;
+export const AGADIR_DRIVER_LOGO_URL = '/agadir-driver-logo-new.png';
 export const HOMEPAGE_HERO_IMAGE_URL = 'https://images.transfeero.com/e024607e-8976-4f97-6fd5-a63356d5ca00/public';
-export const INFO_SECTION_PRIMARY_IMAGE_URL = travelCoupleImg.url;
-export const INFO_SECTION_SECONDARY_IMAGE_URL = luxuryVanImg.url;
+export const INFO_SECTION_PRIMARY_IMAGE_URL = '/travel-couple.jpg';
+export const INFO_SECTION_SECONDARY_IMAGE_URL = '/luxury-van.jpg';
 
 export const VEHICLE_IMAGE_URLS = {
   standard: 'https://images.transfeero.com/41bf3efc-6a0e-4725-6547-d6f88cd68b00/public',
